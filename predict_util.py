e458ac196915f40bf1378bc2d2bf2923076f2323
brewerywiwis <brewder136@hotmail.com>
2022-04-19 01:20:12 +0700

    Updates: server.py 1.fix twice log from logging 2.add cors config

--- predict_util.py
+++ predict_util.py
@@ -9,7 +9,6 @@ import time
 import logging
 
 
-
 class PREDICT_UTIL:
     def __init__(self, model_path: str, s_cnn_path: str):
         """The inference utility class that provides the prediction of the given image
@@ -21,69 +20,76 @@ class PREDICT_UTIL:
         self.model_path = model_path
         self.s_cnn_path = s_cnn_path
         self.device = get_default_device()
-        self.invTrans = transforms.Compose([transforms.Normalize(mean = [ 0., 0., 0. ],
-                                                     std = [ 1/0.229, 1/0.224, 1/0.225 ]),
-                                            transforms.Normalize(mean = [ -0.485, -0.456, -0.406 ],
-                                                     std = [ 1., 1., 1. ]),
+        self.invTrans = transforms.Compose([transforms.Normalize(mean=[0., 0., 0.],
+                                                                 std=[1/0.229, 1/0.224, 1/0.225]),
+                                            transforms.Normalize(mean=[-0.485, -0.456, -0.406],
+                                                                 std=[1., 1., 1.]),
                                             ])
         logger = logging.getLogger('PREDICT_UTIL')
         logger.setLevel(logging.DEBUG)
+        logger.handlers.clear()
         ch = logging.StreamHandler()
         ch.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         ch.setFormatter(formatter)
         logger.addHandler(ch)
+        logger.propagate = False
+
         self.logger = logger
-    
+
     def load_model(self):
         """Loading the model into memory
         """
         try:
             start_time = time.time()
             self.logger.info(f'start loading model')
-            self.model = DB_CNN(device=self.device, options=False, model_path=self.s_cnn_path).to(self.device)
-            self.model.load_state_dict(torch.load(self.model_path, map_location=torch.device(self.device)))
-            self.logger.info(f'Load model complete using {time.time()-start_time} seconds')
+            self.model = DB_CNN(device=self.device, options=False,
+                                model_path=self.s_cnn_path).to(self.device)
+            self.model.load_state_dict(torch.load(
+                self.model_path, map_location=torch.device(self.device)))
+            self.logger.info(
+                f'Load model complete using {time.time()-start_time} seconds')
         except:
             self.logger.warning('Cannot load model')
-        
 
-    def gen_skin_img(self, img, crop_shape = (448, 448)):
+    def gen_skin_img(self, img, crop_shape=(448, 448)):
 
-    
         width, height = img.size
         crop_y = crop_shape[0]
         crop_x = crop_shape[1]
         img_list = []
         if height <= 1.2*crop_y or width <= 1.2*crop_x:
             transform_test2 = torchvision.transforms.Compose([
-                    torchvision.transforms.CenterCrop((448, 448)),                                  
-                    torchvision.transforms.ToTensor(),
-                    torchvision.transforms.Normalize(mean=(0.485, 0.456, 0.406),
-                                                    std=(0.229, 0.224, 0.225))])
+                torchvision.transforms.CenterCrop((448, 448)),
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize(mean=(0.485, 0.456, 0.406),
+                                                 std=(0.229, 0.224, 0.225))])
             return transform_test2(img)
         elif height > width:
             new_width = 1*crop_x
             new_heigth = (new_width/width)*height
             # print(new_width, new_heigth)
             transform_test2 = torchvision.transforms.Compose([
-                    torchvision.transforms.Resize(size=(int(new_heigth), int(new_width))),                            
-                    torchvision.transforms.CenterCrop((448, 448)),                                  
-                    torchvision.transforms.ToTensor(),
-                    torchvision.transforms.Normalize(mean=(0.485, 0.456, 0.406),
-                                                    std=(0.229, 0.224, 0.225))])
+                torchvision.transforms.Resize(
+                    size=(int(new_heigth), int(new_width))),
+                torchvision.transforms.CenterCrop((448, 448)),
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize(mean=(0.485, 0.456, 0.406),
+                                                 std=(0.229, 0.224, 0.225))])
             return transform_test2(img)
         else:
             new_height = 1*crop_y
             new_width = (new_height/height)*width
             transform_test2 = torchvision.transforms.Compose([
-                    torchvision.transforms.Resize(size=(int(new_height), int(new_width))),                             
-                    torchvision.transforms.CenterCrop((448, 448)),                                  
-                    torchvision.transforms.ToTensor(),
-                    torchvision.transforms.Normalize(mean=(0.485, 0.456, 0.406),
-                                                    std=(0.229, 0.224, 0.225))])
+                torchvision.transforms.Resize(
+                    size=(int(new_height), int(new_width))),
+                torchvision.transforms.CenterCrop((448, 448)),
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize(mean=(0.485, 0.456, 0.406),
+                                                 std=(0.229, 0.224, 0.225))])
             return transform_test2(img)
-            
+
     def predict_img(self, IMG_PATH):
         with torch.no_grad():
             self.model.eval()
@@ -96,7 +102,7 @@ class PREDICT_UTIL:
             npimg = self.invTrans(img).cpu().numpy()
             plt.imshow(np.transpose(npimg, (1, 2, 0)))
             return yb.item()
-    
+
     def predict_img_2(self, img):
         start_time = time.time()
         self.logger.info('Start predict')
@@ -105,14 +111,12 @@ class PREDICT_UTIL:
             torch.cuda.empty_cache()
             # print(img)
             img = self.gen_skin_img(img)
-            
+
             xb = to_device(img.unsqueeze(0), self.device)
             yb = self.model(xb)
             # print(yb.item())
             npimg = self.invTrans(img).cpu().numpy()
             # plt.imshow(np.transpose(npimg, (1, 2, 0)))
-            self.logger.info(f'Predict finish using {time.time() - start_time} seconds')
+            self.logger.info(
+                f'Predict finish using {time.time() - start_time} seconds')
             return yb.item()
-
-        
-    
--- server.py
+++ server.py
@@ -1,21 +1,9 @@
 from fastapi import FastAPI, File, UploadFile
 import uvicorn
 from predict_util import *
-import urllib.request
-import ssl
 from io import BytesIO
 import logging
-
-logger = logging.getLogger('PREDICT_UTIL')
-logger.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-ch.setFormatter(formatter)
-logger.addHandler(ch)
-
-ssl._create_default_https_context = ssl._create_unverified_context
-response = urllib.request.urlopen('https://www.python.org')
+from fastapi.middleware.cors import CORSMiddleware
 
 S_CNN_PATH = 'revised_model_13_10_2021_0.pth'
 MODEL_PATH = 'db_cnn_v2_challenge.pth'
@@ -24,10 +12,36 @@ Predict.load_model()
 
 app = FastAPI()
 
+# CONFIG ORIGIN SERVERS HERE
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+logger = logging.getLogger("BIQA_SERVER")
+logger.setLevel(logging.DEBUG)
+logger.handlers.clear()
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.propagate = False
+
+
 def read_imagefile(file) -> Image.Image:
     logger.info('Read image')
     image = Image.open(BytesIO(file))
     return image
+
 
 @app.post("/predict/image")
 async def predict_api(file: UploadFile = File(...)):
